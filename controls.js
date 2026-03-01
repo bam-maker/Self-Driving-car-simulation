b2c88cb46d98e20b9f3de50a84d6bbf314336049
bam-maker <123272575+bam-maker@users.noreply.github.com>
2026-02-28 22:36:25 -0600

    Update

--- controls.js
+++ controls.js
@@ -1,44 +1,51 @@
 class Controls{
-    constructor(){
-        this.forward = false;
-        this.left = false;
-        this.right = false;
-        this.reverse = false;
+    constructor(type){
+        this.forward=false;
+        this.left=false;
+        this.right=false;
+        this.reverse=false;
 
-        this.#addKeyboardListeners();
+        switch(type){
+            case "KEYS":
+                this.#addKeyboardListeners();
+                break;
+            case "DUMMY":
+                this.forward=true;
+                break;
+        }
     }
+
     #addKeyboardListeners(){
         document.onkeydown=(event)=>{
             switch(event.key){
                 case "ArrowLeft":
-                    this.left = true;
+                    this.left=true;
                     break;
                 case "ArrowRight":
-                    this.right = true;
+                    this.right=true;
                     break;
                 case "ArrowUp":
-                    this.forward = true;
-                    break
+                    this.forward=true;
+                    break;
                 case "ArrowDown":
-                    this.reverse = true;
-                    break
+                    this.reverse=true;
+                    break;
             }
-
         }
         document.onkeyup=(event)=>{
             switch(event.key){
                 case "ArrowLeft":
-                    this.left = false;
+                    this.left=false;
                     break;
                 case "ArrowRight":
-                    this.right = false;
+                    this.right=false;
                     break;
                 case "ArrowUp":
-                    this.forward = false;
-                    break
+                    this.forward=false;
+                    break;
                 case "ArrowDown":
-                    this.reverse = false;
-                    break
+                    this.reverse=false;
+                    break;
             }
         }
     }
